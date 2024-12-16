ec9e5ce31d7dae29678f462ed4e5dc021ac05c18
H12nH-Diana01 <harnah.diana01@gmail.com>
2024-12-16 18:57:12 +0700

    first commit

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+function HelloWorld() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Selamat belajar nextjs</p>
+      <button>Login</button>
+    </>
+  )
+}
+
+export default HelloWorld
